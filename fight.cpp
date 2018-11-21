26d8ba212ae051c89092aef1b0adeb11da657e98
Shotokan12 <ryanboydguam@gmail.com>
2018-11-21 16:54:32 +1000

    Update fight.cpp

--- fight.cpp
+++ fight.cpp
@@ -15,8 +15,7 @@ class Fight{
     double fri1 = 0;
     double fri2 = 0;
     double fri3 = 0;
-    double hitfinal = 0;
-    double frifinal = 0;
+   
     
     //Stats hitman
     int str1 = 0;
@@ -33,10 +32,21 @@ class Fight{
     int mag2 = 0;
     int hp2 = 0;
     
+    //active fight information
+     double hitfinal = 0;
+    double frifinal = 0;
+    
+    double activehp1 = 0;
+    double activehp2 = 0;
+    
+    double hitcal1 = 0;
+    double hitcal2 = 0;
+    char toggleh = 'b';
+    
     void combatscript(){
         (srand(time(NULL)));
         //first class calculated
-        if(hitclass == "Melee"){
+        
             str1 = 87;
             str1 = str1 + ((rand()%6)+1);
             str1 = str1 + ((rand()%6)+1);
@@ -53,40 +63,9 @@ class Fight{
             hit1 = (str1+att1+def1);
             hit1 = hit1 + (hp1*3);
             hit1 = hit1 * 0.05050505;
-        };
-        if(hitclass == "Ranger"){
-            ran1 = 87;
-            ran1 = ran1 + ((rand()%6)+1);
-            ran1 = ran1 + ((rand()%6)+1);
-            def1 = 87;
-            def1 = def1 + ((rand()%6)+1);
-            def1 = def1 + ((rand()%6)+1);
-            hp1 = 87;
-            hp1 = hp1 + ((rand()%6)+1);
-            hp1 = hp1 + ((rand()%6)+1);
-            
-            hit2 = (ran1) + (def1*2);
-            hit2 = hit2 + (hp1*3);
-            hit2 = hit2 * (0.05050505);
-        };
-        if(hitclass == "Magic"){
-            mag1 = 87;
-            mag1 = mag1 + ((rand()%6)+1);
-            mag1 = mag1 + ((rand()%6)+1);
-            def1 = 87;
-            def1 = def1 + ((rand()%6)+1);
-            def1 = def1 + ((rand()%6)+1);
-            hp1 = 87;
-            hp1 = hp1 + ((rand()%6)+1);
-            hp1 = hp1 + ((rand()%6)+1);
-            
-            hit3 = (mag1);
-            hit3 = hit3 + (def1*2);
-            hit3 = hit3 +(hp1*3);
-            hit3 = hit3 * (0.05050505);
-        };
+       
         //secound class calculated
-        if(friclass == "Melee"){
+       
            str2 = 87;
             str2 = str2 + ((rand()%6)+1);
             str2 = str2 + ((rand()%6)+1);
@@ -103,63 +82,14 @@ class Fight{
             fri1 = (str2+att2+def2);
             fri1 = fri1 +(hp2*3);
             fri1 = fri1*(0.05050505);
-        };
-        if(friclass == "Ranger"){
-            ran2 = 87;
-            ran2 = ran2 + ((rand()%6)+1);
-            ran2 = ran2 + ((rand()%6)+1);
-            def2 = 87;
-            def2 = def2 + ((rand()%6)+1);
-            def2 = def2 + ((rand()%6)+1);
-            hp2 = 87;
-            hp2 = hp2 + ((rand()%6)+1);
-            hp2 = hp2 + ((rand()%6)+1);
-            fri2 = (ran2);
-            fri2 = fri2+(def2*2);
-            fri2 = fri2+(hp2*3);
-            fri2 = fri2*(0.05050505);
-        };
-        if(friclass == "Magic"){
-            mag2 = 87;
-            mag2 = mag2 + ((rand()%6)+1);
-            mag2 = mag2 + ((rand()%6)+1);
-            def2 = 87;
-            def2 = def2 + ((rand()%6)+1);
-            def2 = def2 + ((rand()%6)+1);
-            hp2 = 87;
-            hp2 = hp2 + ((rand()%6)+1);
-            hp2 = hp2 + ((rand()%6)+1);
-            
-            
-          
-            fri3 = (mag2);
-            fri3 = fri3+(def2*2);
-            fri3 = fri3+(hp2*3);
-            fri3 = fri3*(0.05050505);
-        };
-      
         
-        if(hitclass == "Melee"){
-            hitfinal = hit1;
-            
-        };
-        if(hitclass == "Magic"){
-            hitfinal = hit2;
-            
-        };
-        if(hitclass == "Ranger"){
-            hitfinal = hit3;
-            
-        };
-        if(friclass == "Melee"){
-            frifinal = fri1;
-        };
-        if(friclass == "Magic"){
-            frifinal = fri2;
-        };
-        if(friclass == "Ranger"){
-            frifinal = fri3;
-        };
+       
+        
+        hitfinal = hit1;
+        frifinal = fri1;
+        
+        
+     
         
         cout << "First person has a max hit of "<< hitfinal << endl;
         cout << "Second person has a max hit of "<< frifinal << endl;
@@ -168,10 +98,69 @@ class Fight{
         cout << "Stats of Second person " << str2 <<" || "<< att2 <<" || "<< def2 <<" || "<< ran2 <<" || "<< mag2 <<" || "<< hp2 << endl;
         cout << "Time to Fight!" << endl;
         cout << "========================================" << endl;
+        cout << "Fighter information:" << endl;
+        cout << "Hitman Stats: [" << hitclass << "]" << endl;
+        cout << "Max hit: " << hitfinal << endl;
+        cout << "Combat levels. Attack: " << att1 << " Strenght: " << str1 << " Defence: " << def1 << " Hitpoints: "<< hp1 << endl;
+        cout << "========================================" << endl;
+        cout << "Fighter information:" << endl;
+        cout << "Hitman Stats: [" << friclass << "]" << endl;
+        cout << "Max hit: " << frifinal << endl;
+        cout << "Combat levels. Attack: " << att2 << " Strenght: " << str2 << " Defence: " << def2 << " Hitpoints: "<< hp2 << endl;
+        cout << "========================================" << endl;
+        activehp1 = hp1;
+        activehp2 = hp2;
+        
+        cout << "Enter ! to hit!" << endl;
+        cout << hitclass << " gets the first hit!" << endl;
+        
+        int hitfinalcal = 0;
+        hitfinalcal = hitfinal * 1000;
+        
+        int frifinalcal = 0;
+        frifinalcal = frifinal * 1000;
         
         
         
-    }
+        int stop = 1;
+        int stopattacks = 1;
+        
+        while(stop != 0){
+         
+            if(stopattacks <= 0){
+            hitcal1 =  ((rand()%hitfinalcal)+1);
+            hitcal1 = hitcal1 / 1000;
+            activehp2 = activehp2 - hitcal1;
+            cout << hitclass << " Hits you with a [ " << hitcal1 << " ] Your hitpoints are now at [ " << activehp2 << " ]"<< endl;
+            if(activehp2 <= 0){
+                stopattacks = 0;
+                stop = 0;
+            }
+                
+            }
+            
+            cin >> toggleh;
+            
+            if(stopattacks <= 0){
+            if(toggleh == '!'){
+            hitcal2 = ((rand()%frifinalcal)+1);
+            hitcal2 = hitcal2 / 1000;
+            activehp1 = activehp1 - hitcal2;
+            cout << friclass << " Hits you with a [ " << hitcal2 << " ] Their hitpoints are now at [ " << activehp1 << " ]"<< endl;
+            toggleh = 'b';   
+            }
+            if(activehp1 <= 0){
+                stopattacks = 0;
+                stop = 0;
+            }
+            }
+         
+    };
+        
+ 
+        
+    
+    
     
     
     
